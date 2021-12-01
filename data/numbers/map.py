5dae09ea6a722635e08771312ad5bd830ffbe67c
Wexler <47161750+DanielWexler@users.noreply.github.com>
2021-12-01 13:19:07 -0800

    Add files via upload
    The 1st upload, the start.

---
+++ data/numbers/map.py
@@ -0,0 +1,314 @@
+{
+    1: {
+        'hebrew': 'א',
+        'unicode': ['U+05D0'],
+        'html': ['&#1488;'],
+        'hebrew_say': 'Alef'
+    },
+    2: {
+        'hebrew': 'ב',
+        'unicode': ['U+05D1'],
+        'html': ['&#64305;'],
+        'hebrew_say': 'Bet'
+    },
+    3: {
+        'hebrew': 'ג',
+        'unicode': ['U+05D2'],
+        'html': ['&#1490;'],
+        'hebrew_say': 'Gimel'
+    },
+    4: {
+        'hebrew': 'ד',
+        'unicode': ['U+05D3'],
+        'html': ['&#1491;'],
+        'hebrew_say': 'Dalet'
+    },
+    5: {
+        'hebrew': 'ה',
+        'unicode': ['U+05D4'],
+        'html': ['&#1492;'],
+        'hebrew_say': 'He'
+    },
+    6: {
+        'hebrew': 'ו',
+        'unicode': ['U+05D5'],
+        'html': ['&#1493;'],
+        'hebrew_say': 'Vav'
+    },
+    7: {
+        'hebrew': 'ז',
+        'unicode': ['U+05D6'],
+        'html': ['&#1494;'],
+        'hebrew_say': 'Zayin'
+    },
+    8: {
+        'hebrew': 'ח',
+        'unicode': ['U+05D7'],
+        'html': ['&#1495;'],
+        'hebrew_say': 'Het'
+    },
+    9: {
+        'hebrew': 'ט',
+        'unicode': ['U+05D8'],
+        'html': ['&#1496;'],
+        'hebrew_say': 'Tet'
+    },
+    10: {
+        'hebrew': 'י',
+        'unicode': ['U+05D9'],
+        'html': ['&#1488;'],
+        'hebrew_say': 'Yod'
+    },
+    11: {
+        'hebrew': 'יא',
+        'unicode': ['U+05D9', ],
+        'html': ['&#1488;', ],
+        'hebrew_say': 'Yod Alef'
+    },
+    12: {
+        'hebrew': 'יב',
+        'unicode': ['U+05D9', 'U+05D1'],
+        'html': ['&#1488;', '&#64305;'],
+        'hebrew_say': 'Yod Bet'
+    },
+    13: {
+        'hebrew': 'יג',
+        'unicode': ['U+05D9', 'U+05D2'],
+        'html': ['&#1488;', '&#1490;'],
+        'hebrew_say': 'Yod Gimel'
+    },
+    14: {
+        'hebrew': 'יד',
+        'unicode': ['U+05D9', 'U+05D3'],
+        'html': ['&#1488;', '&#1491;'],
+        'hebrew_say': 'Yod Dalet'
+    },
+    15: {
+        'hebrew': 'טו',
+        'unicode': ['U+05D8', 'U+05D5'],
+        'html': ['&#1488;', '&#1493;'],
+        'hebrew_say': 'Tet Vav'
+    },
+    16: {
+        'hebrew': 'טז',
+        'unicode': ['U+05D8', ],
+        'html': ['&#1488;', '&#1494;'],
+        'hebrew_say': 'Tet Zayin'
+    },
+    17: {
+        'hebrew': 'יז',
+        'unicode': ['U+05D9', 'U+05D6'],
+        'html': ['&#1488;', '&#1494;'],
+        'hebrew_say': 'Yod Zayin'
+    },
+    18: {
+        'hebrew': 'יח',
+        'unicode': ['U+05D9', 'U+05D7'],
+        'html': ['&#1488;', '&#1495;'],
+        'hebrew_say': 'Yod Het'
+    },
+    19: {
+        'hebrew': 'יט',
+        'unicode': ['U+05D9', 'U+05D8'],
+        'html': ['&#1488;', '&#1496;'],
+        'hebrew_say': 'Yod Tet'
+    },
+    20: {
+        'hebrew': 'כ',
+        'unicode': ['U+05DB'],
+        'html': ['&#1488;', ],
+        'hebrew_say': 'Kaf'
+    },
+    21: {
+        'hebrew': 'כא',
+        'unicode': ['U+05DB', 'U+05D0'],
+        'html': ['&#1488;', ],
+        'hebrew_say': 'Kaf Alef'
+    },
+    22: {
+        'hebrew': 'כב',
+        'unicode': ['U+05DB', ],
+        'html': ['&#1488;', '&#64305;'],
+        'hebrew_say': 'Kaf Bet'
+    },
+    23: {
+        'hebrew': 'כג',
+        'unicode': ['U+05DB', 'U+05D2'],
+        'html': ['&#1488;', '&#1490;'],
+        'hebrew_say': 'Kaf Gimel'
+    },
+    24: {
+        'hebrew': 'כד',
+        'unicode': ['U+05DB', 'U+05D3'],
+        'html': ['&#1488;', '&#1491;'],
+        'hebrew_say': 'Kaf Dalet'
+    },
+    25: {
+        'hebrew': 'כה',
+        'unicode': ['U+05DB', 'U+05D4'],
+        'html': ['&#1488;', '&#1492;'],
+        'hebrew_say': 'Kaf He'
+    },
+    26: {
+        'hebrew': 'כו',
+        'unicode': ['U+05DB', 'U+05D5'],
+        'html': ['&#1488;', '&#1493;'],
+        'hebrew_say': 'Kaf Vav'
+    },
+    27: {
+        'hebrew': 'כז',
+        'unicode': ['U+05DB', 'U+05D6'],
+        'html': ['&#1488;', '&#1494;'],
+        'hebrew_say': 'Kaf Zayin'
+    },
+    28: {
+        'hebrew': 'כח',
+        'unicode': ['U+05DB', 'U+05D7'],
+        'html': ['&#1488;', '&#1495;'],
+        'hebrew_say': 'Kaf Het'
+    },
+    29: {
+        'hebrew': 'כט',
+        'unicode': ['U+05DB', 'U+05D8'],
+        'html': ['&#1488;', '&#1496;'],
+        'hebrew_say': 'Kaf Tet'
+    },
+    30: {
+        'hebrew': 'ל',
+        'unicode': ['U+05DC'],
+        'html': ['&#1488;', ],
+        'hebrew_say': 'Lamed'
+    },
+    31: {
+        'hebrew': 'לא',
+        'unicode': ['U+05DC', ],
+        'html': ['&#1488;', ],
+        'hebrew_say': 'Lamed Alef'
+    },
+    32: {
+        'hebrew': 'לב',
+        'unicode': ['U+05DC', ],
+        'html': ['&#1488;', '&#64305;'],
+        'hebrew_say': 'Lamed Bet'
+    },
+    33: {
+        'hebrew': 'לג',
+        'unicode': ['U+05DC', 'U+05D2'],
+        'html': ['&#1488;', '&#1490;'],
+        'hebrew_say': 'Lamed Gimel'
+    },
+    34: {
+        'hebrew': 'לד',
+        'unicode': ['U+05DC', 'U+05D3'],
+        'html': ['&#1488;', '&#1491;'],
+        'hebrew_say': 'Lamed Dalet'
+    },
+    35: {
+        'hebrew': 'לה',
+        'unicode': ['U+05DC', 'U+05D4'],
+        'html': ['&#1488;', '&#1492;'],
+        'hebrew_say': 'Lamed He'
+    },
+    36: {
+        'hebrew': 'לו',
+        'unicode': ['U+05DC', 'U+05D5'],
+        'html': ['&#1488;', '&#1493;'],
+        'hebrew_say': 'Lamed Vav'
+    },
+    37: {
+        'hebrew': 'לז',
+        'unicode': ['U+05DC', 'U+05D6'],
+        'html': ['&#1488;', '&#1494;'],
+        'hebrew_say': 'Lamed Zayin'
+    },
+    38: {
+        'hebrew': 'לח',
+        'unicode': ['U+05DC', 'U+05D7'],
+        'html': ['&#1488;', '&#1495;'],
+        'hebrew_say': 'Lamed Het'
+    },
+    39: {
+        'hebrew': 'לט',
+        'unicode': ['U+05DC', 'U+05D8'],
+        'html': ['&#1488;', '&#1496;'],
+        'hebrew_say': 'Lamed Tet'
+    },
+    40: {
+        'hebrew': 'מ',
+        'unicode': ['U+05DE'],
+        'html': ['&#1488;', ],
+        'hebrew_say': 'Mem'
+    },
+    41: {
+        'hebrew': 'מא',
+        'unicode': ['U+05DE', ],
+        'html': ['&#1488;', ],
+        'hebrew_say': 'Mem Alef'
+    },
+    42: {
+        'hebrew': 'מב',
+        'unicode': ['U+05DE', 'U+05D1'],
+        'html': ['&#1488;', '&#64305;'],
+        'hebrew_say': 'Mem Bet'
+    },
+    43: {
+        'hebrew': 'מג',
+        'unicode': ['U+05DE', 'U+05D2'],
+        'html': ['&#1488;', '&#1490;'],
+        'hebrew_say': 'Mem Gimel'
+    },
+    44: {
+        'hebrew': 'מד',
+        'unicode': ['U+05DE', 'U+05D3'],
+        'html': ['&#1488;', '&#1491;'],
+        'hebrew_say': 'Mem Dalet'
+    },
+    45: {
+        'hebrew': 'מה',
+        'unicode': ['U+05DE', 'U+05D4'],
+        'html': ['&#1488;', '&#1492;'],
+        'hebrew_say': 'Mem He'
+    },
+    46: {
+        'hebrew': 'מו',
+        'unicode': ['U+05DE', 'U+05D5'],
+        'html': ['&#1488;', '&#1493;'],
+        'hebrew_say': 'Mem Vav'
+    },
+    47: {
+        'hebrew': 'מז',
+        'unicode': ['U+05DE', 'U+05D6'],
+        'html': ['&#1488;', '&#1494;'],
+        'hebrew_say': 'Mem Zayin'
+    },
+    48: {
+        'hebrew': 'מח',
+        'unicode': ['U+05DE', 'U+05D7'],
+        'html': ['&#1488;', '&#1495;'],
+        'hebrew_say': 'Mem Het'
+    },
+    49: {
+        'hebrew': 'מט',
+        'unicode': ['U+05DE', 'U+05D8'],
+        'html': ['&#1488;', '&#1496;'],
+        'hebrew_say': 'Mem Tet'
+    },
+    50: {
+        'hebrew': 'נ',
+        'unicode': ['U+05E0'],
+        'html': ['&#1488;', ],
+        'hebrew_say': 'Nun'
+    },
+    51: {
+        'hebrew': 'נא',
+        'unicode': ['U+05E0', 'U+05D0'],
+        'html': ['&#1488;', ],
+        'hebrew_say': 'Nun Alef'
+    },
+    52: {
+        'hebrew': 'נב',
+        'unicode': ['U+05E0', 'U+05D1'],
+        'html': ['&#1488;', '&#64305;'],
+        'hebrew_say': 'Nun Bet'
+    },
+}
